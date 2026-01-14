d9d81cedc7a6bf3677bdb40f8f9a1b6dc23d0e02
Shubham Gupta <justliving251@gmail.com>
2026-01-15 00:46:11 +0530

    2 problems

--- Patterns/two pointers/remove_dupl.cpp
+++ Patterns/two pointers/remove_dupl.cpp
@@ -24,3 +24,80 @@ int removeDuplicates(vector<int>& nums) {
         return ans;
           
     }
+
+
+
+
+
+    /**Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well. */
+
+
+    class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head) return nullptr;
+        ListNode* l = head;
+        ListNode* r = head->next;
+        while(r != NULL){
+            if(l->val == r->val){
+                r= r->next;
+                l->next = NULL;
+            }else if(l->val != r->val){
+                l->next = r;
+                l = r;
+                r=r->next;
+            }
+        }
+
+        return head;
+        
+    }
+};
+
+
+
+/**
+    remove the duplicates from the sorted linked list
+
+    Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+
+ class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int ans = 1;
+        int cnt = 1;
+        int l = 0;
+        int r = 1; 
+        while( r < nums.size()){
+            if(nums[l] == nums[r]){
+                if(cnt < 2){
+                    nums[l+1] = nums[r];
+                    r++;
+                    l++;
+                    cnt++;
+                    ans++;
+
+                }
+                else{r++;}
+                
+            }else if(nums[l] != nums[r]){
+                nums[l+1] = nums[r];
+                l++;
+                r++;
+                cnt=1;
+                ans++;
+
+            }         
+        }
+        cout << ans;
+        return ans;
+
+        
+    }
+};
